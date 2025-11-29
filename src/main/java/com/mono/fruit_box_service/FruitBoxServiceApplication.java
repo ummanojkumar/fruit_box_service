25d2613b5c8f28f4757b779ed2f0004f14671e94
manoj <ummanojkumar925@gmail.com>
2025-11-29 18:48:21 +0530

    intial commit

---
+++ src/main/java/com/mono/fruit_box_service/FruitBoxServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mono.fruit_box_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FruitBoxServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FruitBoxServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mono/fruit_box_service/FruitBoxServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mono.fruit_box_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FruitBoxServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
